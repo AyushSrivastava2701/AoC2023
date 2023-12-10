7e26f6dfb2ad572c7c974211a2b2c3d935535a93
Ayush Srivastava <ayush.srivastava@ansys.com>
2023-12-10 15:16:15 +0530

    Day 8 solution part 2

--- Day8.2.py
+++ Day8.2.py
@@ -1,45 +1,30 @@
-from collections import defaultdict
-import re
-inp = list()
+from math import gcd
+map = dict()
 with open('input.txt','r') as fr:
+    nav = [0 if i=='L' else 1 for i in fr.readline()[:-1]]
+    N = len(nav)
+    _=fr.readline()
     for l in fr:
-        c,b = l.split()
-        inp.append((c,int(b)))
+        pn,_,lc,rc = l.split()
+        lc = lc[1:-1]; rc = rc[:-1]
+        map[pn] = (lc,rc)
+# print(nav)
+# print(map)
 
-# print(inp)
-card_rank = ['A', 'K', 'Q', 'T', '9', '8', '7', '6', '5', '4', '3', '2','J']
-card_rank.reverse()
+def get_steps(cn,map):
+    steps = 0
+    vn = list()
+    while cn[-1] != 'Z':
+        vn.append(cn)
+        dir = nav[steps%N]
+        steps += 1
+        cn = map[cn][dir]
+    return steps
 
-
-def get_bundle_category(cs):
-    cn= defaultdict(int)
-    for c in cs: cn[c]+=1
-    cns = sorted([i for i in cn.values()])
-    if len(cns) == 1: return 7*(13**6)
-    if len(cns) == 2 and 4 in cns: return 6*(13**6)
-    if len(cns) == 2 and 3 in cns: return 5*(13**6)
-    if len(cns) == 3 and 3 in cns: return 4*(13**6)
-    if len(cns) == 3 and 2 in cns: return 3*(13**6)
-    if len(cns) == 4: return 2*(13**6)
-    return 1*(13**6)
-
-def get_card_number(cs):
-    a = 0
-    for i,c in enumerate(cs[::-1]): a += (13**i)*(card_rank.index(c))
-    return a
-
-def get_card_rank(cs):
-    max_rank = 0
-    for i in card_rank:
-        cr = get_bundle_category(re.sub('J',i,cs))
-        max_rank = max(cr,max_rank)
-    return max_rank + get_card_number(cs)
-
-# print(get_card_number('AKAAA'))
-# AAAAA -> 371292
-
-# for i in inp: print(f'{i[0]}->{get_bundle_category(i[0])}')
-card_soretd = sorted(inp,key=lambda i: get_card_rank(i[0]))
-ans = sum((i+1)*n[1] for i,n in enumerate(card_soretd))
-print(ans)
-# print(card_soretd)
+sns = [k for k in map.keys() if k[-1]=='A']
+steps = [get_steps(sn,map) for sn in sns]
+# print(steps)
+lcm= 1
+for s in steps:
+    lcm = lcm*s//gcd(lcm,s)
+print(lcm)
